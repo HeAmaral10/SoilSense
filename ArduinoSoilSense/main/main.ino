3b8e0d42e0b301c87616df2d13164442b4259c30
HeAmaral10 <hefacebook100@gmail.com>
2025-08-25 15:12:13 -0300

    Alteração no código do Arduino

--- ArduinoSoilSense/main/main.ino
+++ ArduinoSoilSense/main/main.ino
@@ -10,8 +10,7 @@ int sensor_temperatura = 2;
 OneWire oneWire(sensor_temperatura);
 DallasTemperature sensor_t(&oneWire);
 DeviceAddress endereco_temp;
-int valorUmidade, valorLuz, valorTemperatura, umidade, luz;
-float temperatura;
+int valorUmidade, valorLuz, vumidade, luz;
 
 void setup()
 {
@@ -21,27 +20,24 @@ void setup()
 
 void loop()
 {
-  /*sensor_t.requestTemperatures();
-  if(!sensor_t.getAddress(endereco_temp, 0)) {
-    Serial.println("Sensor não conectado");
-  } else {
-    Serial.print("Temperatura: ");
-    Serial.print(sensor_t.getTempC(endereco_temp, 1));
-  }*/
-
+  
   valorUmidade = analogRead(sensor_umidade);
   valorLuz = analogRead(sensor_ldr);
   sensor_t.requestTemperatures();
   umidade = map(valorUmidade, 0, 1023, 100, 0);
-  temperatura = (valorTemperatura * 5.0 * 100.0) / 1024.0;
   luz = map(valorLuz,54, 974, 100, 0);
   
   Serial.print("Umidade: ");
   Serial.print(umidade);
   Serial.print("% | Lux: ");
   Serial.print(luz);
-  Serial.print(" lx | Temperatura: ");
-  Serial.print(sensor_t.getTempC(endereco_temp, 1));
+  Serial.print(" lx | ");
+  if(!sensor_t.getAddress(endereco_temp, 0)) {
+    Serial.println("Sensor não conectado");
+  } else {
+    Serial.print("Temperatura: ");
+    Serial.print(sensor_t.getTempC(endereco_temp, 1));
+  }
   Serial.print(" ");
   Serial.write(176);
   Serial.println("C");
