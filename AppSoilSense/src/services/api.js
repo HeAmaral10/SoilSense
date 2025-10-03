e99258601634d6dcc28dd198a5e15ddc97973845
HENRIQUE PROFESSOR DE AMARAL <henrique.amaral@etec.sp.gov.br>
2025-10-03 11:25:40 -0300

    Remoção dos aplicativos anteriores

--- AppSoilSense/src/services/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:4000', // ou seu IP local se for no celular
-});
-
-export default api;
--- AppSoilSense/src/tests/math.test.js
+++
@@ -1,5 +0,0 @@
-const sum = require('./math');
-
-test('soma 1 + 2 para ser 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
